52ed574e8bd88cdf663b511908b7430c6f3b6ef8
demas1023 <d.hernandez@fpfempa.es>
2026-01-27 00:28:29 +0100

    intital proyect commit

---
+++ dcpa/src/main/java/com/demas/dcpa/DcpaApplication.java
@@ -0,0 +1,13 @@
+package com.demas.dcpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcpaApplication.class, args);
+	}
+
+}
---
+++ dcpa/src/test/java/com/demas/dcpa/DcpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demas.dcpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DcpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
